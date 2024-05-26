205b4803722d07acb4412f2cc0f4d12fa278e600
MrSmileyJr0 <153967447+MrSmileyJr0@users.noreply.github.com>
2024-05-25 20:07:59 -0700

    Ep. 22

---
+++ neuralnetwork/src/matrix/Matrix.java
@@ -0,0 +1,49 @@
+package orion.matrix;
+
+public class Matrix {
+
+	private static final String NUMBER_FORMAT = "%12.5f";
+
+	private int rows;
+	private int cols;
+
+	public interface Producer {
+		double produce(int index);
+	}
+
+	private double[] a;
+
+	public Matrix(int rows, int cols) {
+
+		this.rows = rows;
+		this.cols = cols;
+
+		a = new double[rows * cols];
+	}
+
+	public Matrix(int rows, int cols, Producer producer) {
+		this(rows, cols);
+
+		for(int i = 0; i < a.length; i++) {
+			a[i] = producer.produce(i);
+		}
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+
+		int index = 0;
+
+		for(int row = 0; row < rows; row++) {
+			for(int col = 0; col < cols; col++) {
+				sb.append(String.format(NUMBER_FORMAT, a[index]));
+
+				index++;
+			}
+
+			sb.append("\n");
+		}
+
+		return sb.toString();
+	}
+}
